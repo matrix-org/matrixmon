f1df02a2b1fa75bfc4f8cde2487ec31dd1ba0212
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2016-01-19 18:04:30 +0000

    Message send/receive roundtrip timers

--- mon.pl
+++ mon.pl
@@ -8,6 +8,8 @@ use IO::Async::Loop;
 
 use Net::Async::Matrix;
 
+use Struct::Dumb;
+use Time::HiRes qw( time gettimeofday tv_interval );
 use YAML qw( LoadFile );
 
 my $CONFIG = LoadFile( "mon.yaml" );
@@ -34,13 +36,42 @@ my $room = $matrix->start
 
 say "Got room";
 
+my $next_txn_id;
+
+# TODO: Work out how to stop this growing without bounds...
+my %txns;
+
+struct Txn => [qw( recv_f )];
+
 $room->configure(
    on_message => sub {
-      my ( undef, $member, $content ) = @_;
-      say "Received message $content->{body}";
+      my ( undef, $member, $content, $event ) = @_;
+      my $txn_id = $content->{txn_id};
+      my $txn = $txns{$txn_id} or return;
+      $txn->recv_f->done( $event );
    },
 );
 
-$room->send_message( "Ping at " . time() )->get;
+my $txn_id = $next_txn_id++;
 
-$loop->delay_future( after => 20 )->get;
+my $txn = $txns{$txn_id} = Txn(
+   my $recv_f = Future->new,
+);
+
+my $start = [ gettimeofday ];
+
+# TODO: deadline, cancellation on failure, etc...
+
+Future->needs_all(
+   $room->send_message(
+      type => "m.text",
+      body => "Ping at " . time(),
+      txn_id => $txn_id,
+   )->on_done( sub {
+      say "Sent in " . tv_interval( $start );
+   }),
+
+   $recv_f->on_done( sub {
+      say "Received in " . tv_interval( $start );
+   }),
+)->get;
